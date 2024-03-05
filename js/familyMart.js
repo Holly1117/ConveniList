60f630da03f98b45aed43eac006a2e6233bda4b0
Holly1117 <79395160+Holly1117@users.noreply.github.com>
2024-03-05 23:23:32 +0900

    旧コンビニリストのデータを削除

--- js/familyMart.js
+++ js/familyMart.js
@@ -3,13 +3,13 @@ window.onload = function () {
 }
 
 function setCard() {
-    var $result = $(".card-list");
+    var $result = $(".familymart");
     var cardList = [];
-    $.getJSON("./familyMart.json", function (dataList) {
+    $.getJSON("/familyMart.json", function (dataList) {
         for (var i in dataList) {
             var imageTag = `<img class=\"card-img\" src=\"${dataList[i].product_image}\">`;
             var titleTag = `<p class=\"card-title\">${dataList[i].product_name}</p>`;
-            var cardTextTag = `<div class=\"card-text\"><p>${dataList[i].product_price}円(税込)</p><p>発売日${dataList[i].product_date}</p></div>`;
+            var cardTextTag = `<div class=\"card-text\"><p>${dataList[i].product_price}円<span>(税込)</span></p><p>発売日<span class="release">${dataList[i].product_date}</span></p></div>`;
             var cardContentTag = `<div class=\"card\">${imageTag}<div class=\"card-content\">${titleTag + cardTextTag}</div></div>`;
             cardList.push(`<a href=\"${dataList[i].product_url}\">${cardContentTag}</a>`);
         }
